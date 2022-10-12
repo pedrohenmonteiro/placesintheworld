7d21ef690810ff25cbfc762e4500aa3fd56fa16c
pedrohenmonteiro <pedromont436@gmail.com>
2022-10-12 20:26:06 -0300

    First commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>My Trips</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"
+          integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14="
+          crossOrigin=""
+        />
+        <meta name="theme-color" content="#06092B" />
+        <meta
+          name="description"
+          content="A simple project to show my favorite spots in the world"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
